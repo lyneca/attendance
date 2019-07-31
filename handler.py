c50dc4152cb9966bea9efd4055d79e458839cd21
Luke Tuthill <lukemtuthill@gmail.com>
2019-07-31 21:07:16 +1000

    Add and merge handler code from old codebase

--- handler.py
+++ handler.py
@@ -1,7 +1,68 @@
+import requests
+import time
+import logger
+from datetime import datetime
+
 class Handler:
     def __init__(self, config):
-        ...
+        self.config = config
+        self.assignments = None
 
-    def send(self, data):
+    def get_headers(self):
+        return {
+            "Authorization": "Bearer " + self.config.access_token,
+            "Content-Type": "multipart/form-data"
+        }
+
+
+    def get(self, url):
+        """Send a GET request to the Canvas server"""
+        return requests.get(self.config.server + url, headers=self.get_headers())
+
+    def get_assignments(self):
+        """Attempt to request the list of attendance assignments"""
+        while True:
+            try:
+                logger.info("Getting list of assignments")
+                self.assignments = self.get("/courses/{}/assignments?per_page=50".format(
+                    self.config.course)).json()
+                break
+            except requests.exceptions.ConnectionError:
+                logger.error("Could not connect to Canvas")
+                time.sleep(3)
+                continue
+        logger.info("Successfully requested assignment list")
+
+    def get_aid(self):
+        """Get the assignment corresponding to this date"""
+        if not self.assignments:
+            return None
+        date = datetime.now().isoformat().split("T")[0]
+        aids = [
+            x for x in self.assignments if x["name"].startswith("Attendance")
+            and date == x["due_at"].split("T")[0]
+        ]
+        if aids:
+            logger.info("Got AID:", aids[0]["id"])
+            return aids[0]["id"]
+        return None
+
+    def send(self, sid):
         """Attempt to send scanned card data to Canvas"""
-        ...
+        aid = self.get_aid()
+        logger.info("Sending", sid)
+
+        if aid:
+            url = self.config.server + \
+                "/courses/{}/assignments/{}/submissions/sis_user_id:{}".format(
+                    self.config.course, aid, sid
+                )
+            response = requests.put(
+                url, "submission[posted_grade]=1", headers=self.get_headers())
+            if response.ok:
+                logger.info("Sent")
+            else:
+                logger.error(response.status_code, response.reason)
+                print(response.content)
+        else:
+            logger.error("No assignment found.")
