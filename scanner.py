072d3d6b835278c7f07b60b6e32b2316f5980ce3
Luke Tuthill <lukemtuthill@gmail.com>
2019-07-31 10:25:15 +1000

    Add RFID reader code

--- scanner.py
+++ scanner.py
@@ -1,4 +1,7 @@
+import time
+
 import logger
+import buzzer
 
 try:
     from pirc522 import RFID
@@ -9,14 +12,55 @@ KEY = []
 
 class Scanner:
     def __init__(self):
-        ...
+        self.rdr = RFID()
+        self.util = self.rdr.util()
+        self.util.debug = True
+        self.last_sid = 0
+        self.last_time = 0
+
 
     def scan(self):
         """Attempt to scan a card
 
         Returns None if no card can be scanned.
         """
-        if False:
-            return True
-        return None
 
+        logger.info("Ready.")
+
+        buzzer.setup_complete()
+
+        self.rdr.wait_for_tag()
+        (e, tag_type) = self.rdr.request()
+        if e:
+            logger.error("Could not request tag")
+            buzzer.error()
+            return
+        logger.info("Tag found")
+        (e, uid) = self.rdr.anticoll()
+        if e:
+            logger.error("Error in anticollision")
+            buzzer.error()
+            return
+        logger.info("UID:", uid)
+        if self.rdr.select_tag(uid):
+            logger.error("Could not select tag")
+            buzzer.error()
+            return
+        logger.info("Selected tag")
+        if self.rdr.card_auth(self.rdr.auth_a, 12, KEY, uid):
+            logger.error("Could not authenticate")
+            buzzer.error()
+            self.rdr.stop_crypto()
+            return
+        logger.info("Authenticated")
+        data = self.rdr.read(12)[1]
+        sid = ''.join([chr(x) for x in data]).strip()
+        logger.info("SID:", sid)
+        if sid and sid != last_sid or time.time() - last_time > 3:
+            last_sid = sid
+            last_time = time.time()
+            self.rdr.stop_crypto()
+            return sid
+        else:
+            logger.error("Scan collision")
+            buzzer.error()
