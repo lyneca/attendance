c69160fd1c51ea13ad629b7a0122298c9d0566e6
Luke Tuthill <lukemtuthill@gmail.com>
2019-08-02 17:23:17 +1000

    Factor buzzer into logger class

--- logger.py
+++ logger.py
@@ -1,29 +1,34 @@
+def print_prefix(self, code, *msg):
+    """Print a message with a prefix"""
+    print("[{: >4}]".format(code), *msg)
+
 class Logger:
-    def __init__(self, level=1):
+    """Class for handling logging"""
+    def __init__(self, buzzer, level=1):
+        self.buzzer = buzzer
         self.level = level
 
-    def print_prefix(self, code, *msg):
-        print("[{: >4}]".format(code), *msg)
+    def silly(self, *msg):
+        """Log a silly-level message"""
+        if self.level > 0:
+            return
+        print_prefix("SILL", *msg)
 
     def info(self, *msg):
         """Log an info message"""
         if self.level > 1:
             return
-        self.print_prefix("INFO", *msg)
+        print_prefix("INFO", *msg)
 
     def warn(self, *msg):
         """Log a warning message"""
         if self.level > 2:
             return
-        self.print_prefix("WARN", *msg)
+        print_prefix("WARN", *msg)
 
     def error(self, *msg):
         """Log an error message"""
         if self.level > 3:
             return
-        self.print_prefix("ERR", *msg)
-
-    def silly(self, *msg):
-        if self.level > 0:
-            return
-        self.print_prefix("SILL", *msg)
+        self.buzzer.error()
+        print_prefix("ERR", *msg)
