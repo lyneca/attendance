9de3095147022321822e4ef840ec779d8b346063
Luke Tuthill <lukemtuthill@gmail.com>
2019-08-02 17:22:56 +1000

    Add buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,8 +1,59 @@
-def setup_error():
-    ...
+import time
 
-def setup_complete():
-    ...
+#pylint: disable=import-error
+import RPi.GPIO as GPIO
 
-def error():
-    ...
+PIN = 15
+FREQ_LOW = 400
+FREQ_MED = 600
+FREQ_HIGH = 800
+
+SMALL_DELAY = 0.01
+LONG_DELAY = 0.1
+
+
+class Buzzer:
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PIN, GPIO.OUT)
+        self.buzzer = GPIO.PWM(15, FREQ_LOW)
+
+    def buzz(self, freq):
+        self.buzzer.ChangeFrequency(freq)
+        self.buzzer.start(1)
+        time.sleep(LONG_DELAY)
+        self.buzzer.stop()
+        time.sleep(SMALL_DELAY)
+
+    def success(self):
+        """Generic success buzz"""
+        self.buzz(FREQ_LOW)
+        self.buzz(FREQ_HIGH)
+
+    def error(self):
+        """Generic error buzz"""
+        self.buzz(FREQ_HIGH)
+        self.buzz(FREQ_LOW)
+
+    def setup_error(self):
+        """Called when the device errored in the setup stage"""
+        self.buzz(FREQ_HIGH)
+        self.buzz(FREQ_MED)
+        self.buzz(FREQ_LOW)
+
+    def setup_complete(self):
+        """Called when the device is ready to process cards"""
+        self.buzz(FREQ_LOW)
+        self.buzz(FREQ_MED)
+        self.buzz(FREQ_HIGH)
+
+
+if __name__ == '__main__':
+    buzzer = Buzzer()
+    buzzer.success()
+    time.sleep(1)
+    buzzer.error()
+    time.sleep(1)
+    buzzer.setup_complete()
+    time.sleep(1)
+    buzzer.setup_error()
