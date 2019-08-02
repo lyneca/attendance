9f282edfdd4b0333fd5a560dd1e868d3ffdbc89f
Luke Tuthill <lukemtuthill@gmail.com>
2019-08-02 12:33:47 +1000

    Use data pulled from Firebase

--- config.py
+++ config.py
@@ -6,14 +6,18 @@ class Config:
         with open("/home/pi/id") as id_file:
             self.bot_id = int(id_file.read())
         self.server = ""
-        self.course_id = ""
+        self.course = ""
         self.access_token = ""
 
     def update_config(self):
         """Pull config data from Firebase"""
         logger.info("Pulling config from Firebase")
-        requests.post(
+        response = requests.post(
             "https://us-central1-usyd-attendance.cloudfunctions.net/getConfig",
             { "bot_id": self.bot_id }
-        )
+        ).json()
+        config = response['config']
+        self.server = config['server']
+        self.course = config['server']
+        self.access_token = config['server']
         return True
